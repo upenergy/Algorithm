42153ff3f8202d882fce9303492fca1e403884b6
Austin <arrayq@protonmail.com>
2022-06-14 10:43:52 -0700

    JDK Installation

--- src/main/java/org/universe/algorithm/HelloWorld.java
+++ src/main/java/org/universe/algorithm/HelloWorld.java
@@ -2,6 +2,10 @@ package org.universe.algorithm;
 
 public class HelloWorld {
 
+    /**
+     * .;%JAVA_HOME%\jre\lib\rt.jar;%JAVA_HOME%\lib\dt.jar;%JAVA_HOME%\lib\tools.jar;
+     * @param args
+     */
     public static void main(String[] args) {
         int[] array = {5, 9, 1, 9, 5, 3, 7, 6, 1};
     }
