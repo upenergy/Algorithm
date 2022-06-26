45f7720540e1a4a4ccd7d1673d8f25dcb30484a0
arrayq <arrayq@protonmail.com>
2022-06-25 23:31:02 -0700

    Update Heap sort

--- src/main/java/org/universe/algorithm/HeapSort.java
+++ src/main/java/org/universe/algorithm/HeapSort.java
@@ -1,2 +1,49 @@
-package org.universe.algorithm;public class HeapSort {
+package org.universe.algorithm;
+
+public class HeapSort {
+
+	public static void heapify(int[] array, int length, int i) {
+	    int left = 2 * i + 1;
+	    int right = 2 * i + 2;
+	    int largest = i;
+	    if (left < length && array[left] > array[largest]) {
+	        largest = left;
+	    }
+	    if (right < length && array[right] > array[largest]) {
+	        largest = right;
+	    }
+	    if (largest != i) {
+	        int tmp = array[i];
+	        array[i] = array[largest];
+	        array[largest] = tmp;
+	        heapify(array, length, largest);
+	    }
+	}
+
+	public static void heapSort(int[] array) {
+	    if (array.length == 0) {
+	        return;
+	    }
+	    
+	    int length = array.length;
+	    
+	    // Moving from the first element that isn't a leaf towards the root
+	    for (int i = length / 2 - 1; i >= 0; i--) {
+	        heapify(array, length, i);
+	    }
+	    
+	    for (int i = length - 1; i >= 0; i--) {
+	        int tmp = array[0];
+	        array[0] = array[i];
+	        array[i] = tmp;
+	        heapify(array, i, 0);
+	    }
+	}
+
+
+	public static void main(String[] args){
+	    int[] array = {25, 12, 6, 41, 18, 31, 53};
+	    heapSort(array);
+	    System.out.println(Arrays.toString(array));
+	}
 }
