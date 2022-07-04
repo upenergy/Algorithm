e50fac7e96f84a406b563c209043da7aa78ceac2
arrayq <arrayq@protonmail.com>
2022-07-04 10:07:49 -0700

    Update Bubblesort

--- src/main/java/org/universe/algorithm/BubbleSort.java
+++ src/main/java/org/universe/algorithm/BubbleSort.java
@@ -7,7 +7,7 @@ public class BubbleSort {
         int[] arr = Arrays.copyOf(sourceArray, sourceArray.length);
 
         for (int i = 1; i < arr.length; i++) {
-            //设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成。
+            //设定一个标记，若为true，则表示此次循环没有进行交换，也就是待排序列已经有序，排序已经完成
             boolean flag = true;
 
             for (int j = 0; j < arr.length - i; j++) {
