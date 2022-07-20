d03ea7afe9626d3b8478f17f1c5966e072da0a60
arrayq <arrayq@protonmail.com>
2022-07-20 08:52:22 -0700

    Update Merge sort

--- src/main/java/org/universe/algorithm/MergeSort.java
+++ src/main/java/org/universe/algorithm/MergeSort.java
@@ -2,8 +2,7 @@ public class MergeSort {
     // Merges two subarrays of arr[].
     // First subarray is arr[l..m]
     // Second subarray is arr[m+1..r]
-    void merge(int arr[], int l, int m, int r)
-    {
+    public void merge(int arr[], int l, int m, int r) {
         // Find sizes of two subarrays to be merged
         int n1 = m - l + 1;
         int n2 = r - m;
