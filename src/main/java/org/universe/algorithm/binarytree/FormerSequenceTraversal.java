94e46ea4fa85dee1bca46a789b1208ad7c385635
arrayq <arrayq@protonmail.com>
2022-08-08 10:13:28 -0700

    Update

---
+++ src/main/java/org/universe/algorithm/binarytree/FormerSequenceTraversal.java
@@ -0,0 +1,12 @@
+package org.universe.algorithm.binarytree;
+
+public class FormerSequenceTraversal {
+
+    public void preOrderTraverse1(TreeNode root) {
+        if (root != null) {
+            System.out.print(root.val + "->");
+            preOrderTraverse1(root.left);
+            preOrderTraverse1(root.right);
+        }
+    }
+}
