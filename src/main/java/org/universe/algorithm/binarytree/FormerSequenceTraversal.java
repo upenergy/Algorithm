ae45582d958d419aa8a03700635477be94bfb0f1
arrayq <arrayq@protonmail.com>
2022-08-09 10:13:02 -0700

    Update FormerSequenceTraversal

--- src/main/java/org/universe/algorithm/binarytree/FormerSequenceTraversal.java
+++ src/main/java/org/universe/algorithm/binarytree/FormerSequenceTraversal.java
@@ -9,4 +9,20 @@ public class FormerSequenceTraversal {
             preOrderTraverse1(root.right);
         }
     }
+
+    public void preOrderTraverse2(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode node = root;
+        while (node != null || !stack.empty()) {
+            if (node != null) {
+                System.out.print(node.val + "->");
+                stack.push(node);
+                node = node.left;
+            } else {
+                TreeNode tem = stack.pop();
+                node = tem.right;
+            }
+        }
+    }
+
 }
