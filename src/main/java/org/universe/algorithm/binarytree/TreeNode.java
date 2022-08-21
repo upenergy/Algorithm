4efd97a8678fd4cd721e4c46e5137b4f97c592ee
arrayq <arrayq@protonmail.com>
2022-08-21 01:39:10 -0700

    Update TreeNOde

--- src/main/java/org/universe/algorithm/binarytree/TreeNode.java
+++ src/main/java/org/universe/algorithm/binarytree/TreeNode.java
@@ -1,5 +1,7 @@
 package org.universe.algorithm.binarytree;
 
+import java.util.List;
+
 public class TreeNode {
         public int val;
         public TreeNode left;
@@ -11,6 +13,29 @@ public class TreeNode {
             this.left = left;
             this.right = right;
         }
+
+    public TreeNode sortedListToBST(List<Integer> list) {
+        if(list.size() <= 0) {
+            return null;
+        }
+        return generateTree(list, 0, list.size()-1);
+    }
+
+    private TreeNode generateTree(List<Integer> list, int start, int end) {
+        //此时没有数字，将 null 加入结果中
+        if(start > end) {
+            return null;
+        }
+        // root 节点
+        // 1 2 3 4 5
+        int rootIndex = (start + end)/2;
+        int rootVal = list.get(rootIndex);
+        TreeNode treeNode = new TreeNode(rootVal);
+        // left
+        treeNode.left = generateTree(list, start, rootIndex-1);
+        // right
+        treeNode.right = generateTree(list, rootIndex+1, end);
+        return treeNode;
 
     }
 }
