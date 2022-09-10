1f4a385d0055208769450dec32312bd1d640563a
Arrayq <arrayq@protonmail.com>
2022-09-10 00:34:01 -0700

    Append LinkedList

---
+++ src/main/java/org/universe/algorithm/linkedlist/LinkedList.java
@@ -0,0 +1,16 @@
+
+
+public class LinkedList {
+  // Creating a node
+  Node head;
+
+  static class Node {
+    int value;
+    Node next;
+
+    Node(int d) {
+      value = d;
+      next = null;
+    }
+  }
+}
