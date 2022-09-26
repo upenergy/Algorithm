a11ab2c1a0f6632e8dcb43831cd4b596d9b1a976
Arrayq <arrayq@protonmail.com>
2022-09-26 09:54:12 -0700

    Update DoublyLinkedList

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -215,7 +215,50 @@ public class DoublyLinkedList {
         }
     }
 
-
+    public void pairSum( Node head, int x) {
+        // Set two pointers, first
+        // to the beginning of DLL
+        // and second to the end of DLL.
+        Node first = head;
+        Node second = head;
+        while (second.next != null)
+            second = second.next;
+     
+        // To track if we find a pair or not
+        boolean found = false;
+     
+        // The loop terminates when 
+        // they cross each other (second.next
+        // == first), or they become same
+        // (first == second)
+        while ( first != second && second.next != first)
+        {
+            // pair found
+            if ((first.data + second.data) == x)
+            {
+                found = true;
+                System.out.println( "(" + first.data +
+                                    ", "+ second.data + ")" );
+     
+                // move first in forward direction
+                first = first.next;
+     
+                // move second in backward direction
+                second = second.prev;
+            }
+            else
+            {
+                if ((first.data + second.data) < x)
+                    first = first.next;
+                else
+                    second = second.prev;
+            }
+        }
+     
+        // if pair is not present
+        if (found == false)
+            System.out.println("No pair found");
+    }
     
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
