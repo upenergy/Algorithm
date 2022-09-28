cf27c544a5d1f25a50dc49bf1b8c76f5a201f20b
Arrayq <arrayq@protonmail.com>
2022-09-28 10:21:01 -0700

    Update DoublyLinkedList

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -275,6 +275,49 @@ public class DoublyLinkedList {
         }
         return temp;
     }
+
+
+    public Node sortedInsert(Node head, Node newNode) {
+            Node current;
+ 
+            // if list is empty
+            if (head == null)
+                head = newNode;
+ 
+            // if the node is to be inserted at the beginning
+            // of the doubly linked list
+            else if (head.data >= newNode.data)
+            {
+                newNode.next = head;
+                newNode.next.prev = newNode;
+                head = newNode;
+            }
+ 
+            else
+            {
+                current = head;
+ 
+                // locate the node after which the new node
+                // is to be inserted
+                while (current.next != null &&
+                        current.next.data < newNode.data)
+                    current = current.next;
+ 
+                /* Make the appropriate links */
+                newNode.next = current.next;
+ 
+                // if the new node is not inserted
+                // at the end of the list
+                if (current.next != null)
+                    newNode.next.prev = newNode;
+ 
+                current.next = newNode;
+                newNode.prev = current;
+             
+            }
+            return head;
+    }
+    
     
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
