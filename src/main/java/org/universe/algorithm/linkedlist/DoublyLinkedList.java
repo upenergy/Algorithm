6a15df0e48430d0cb734916e14acce5995a2ca11
Arrayq <arrayq@protonmail.com>
2022-10-08 10:12:06 -0700

    Update DoublyLinkedList

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -430,6 +430,33 @@ public class DoublyLinkedList {
           
     }
 
+    /* Function to delete a node in a Doubly Linked List.
+    head_ref --> pointer to head node pointer.
+    del  -->  pointer to node to be deleted. */
+    public void deleteNode(Node head, Node del) {
+         /* base case */
+        if(head==null || del==null)
+        {
+            return ;
+        }
+        /* If node to be deleted is head node */
+        if(head==del)
+        {
+            head=del.next;
+        }
+        /* Change next only if node to be deleted 
+        is NOT the last node */
+        if(del.next!=null)
+        {
+            del.next.prev=del.prev;
+        }
+        /* Change prev only if node to be deleted 
+       is NOT the first node */
+        if (del.prev != null)
+            del.prev.next = del.next;
+ 
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
