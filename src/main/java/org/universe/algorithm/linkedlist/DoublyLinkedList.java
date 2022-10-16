bb5f7a7442dadabab2cca2285ea6954f0592dd3e
Arrayq <arrayq@protonmail.com>
2022-10-16 02:01:36 -0700

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -608,6 +608,28 @@ public class DoublyLinkedList {
         }
     }
 
+    public Node deleteNode(Node head_ref, Node del) {
+        // base case
+        if (head_ref == null || del == null)
+            return head_ref;
+    
+        // If node to be deleted is head node
+        if (head_ref == del)
+            head_ref = del.next;
+    
+        // Change next only if node to be deleted
+        // is NOT the last node
+        if (del.next != null)
+            del.next.prev = del.prev;
+    
+        // Change prev only if node to be deleted
+        // is NOT the first node
+        if (del.prev != null)
+            del.prev.next = del.next;
+        return head_ref;
+    }
+
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
