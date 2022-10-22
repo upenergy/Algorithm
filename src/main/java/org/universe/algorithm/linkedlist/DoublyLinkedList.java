07d12a6b6edbcfb87e9ea379a51c27981e3d9c88
Arrayq <arrayq@protonmail.com>
2022-10-22 08:00:57 -0700

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -666,6 +666,30 @@ public class DoublyLinkedList {
         }
     }
 
+
+    public void push(struct Node** head_ref, int new_data) {
+        // allocate node
+        struct Node* new_node =
+            (struct Node*)malloc(sizeof(struct Node));
+    
+        // put in the data
+        new_node->data = new_data;
+    
+        // since we are adding at the beginning,
+        // prev is always NULL
+        new_node->prev = NULL;
+    
+        // link the old list off the new node
+        new_node->next = (*head_ref);
+    
+        // change prev of head node to new node
+        if ((*head_ref) != NULL)
+            (*head_ref)->prev = new_node;
+    
+        // move the head to point to the new node
+        (*head_ref) = new_node;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
