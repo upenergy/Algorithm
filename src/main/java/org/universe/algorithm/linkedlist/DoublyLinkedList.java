e627ba9e705022332b21351f33b29c9908c57afa
Arrayq <arrayq@protonmail.com>
2022-11-07 10:17:54 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -837,6 +837,17 @@ public class DoublyLinkedList {
         return head;
     }
 
+    public Node push(Node head_ref, char new_data) {
+        Node new_node = new Node();
+        new_node.data = new_data;
+        new_node.next = head_ref;
+        new_node.prev = null;
+        if (head_ref != null)
+        head_ref.prev = new_node ;
+        head_ref = new_node;
+        return head_ref;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
