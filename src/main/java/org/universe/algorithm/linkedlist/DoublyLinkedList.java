f1e140fbc86578345af4ce80805da350cd45204b
Arrayq <arrayq@protonmail.com>
2022-11-12 06:26:18 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -922,6 +922,25 @@ public class DoublyLinkedList {
         next.prev = new_node;
     }
 
+    public void insertBegin(int value) {
+        // Pointer points to last Node
+        Node last = (start).prev;
+  
+        Node new_node = new Node();
+        new_node.data = value; // Inserting the data
+  
+        // setting up previous and next of new node
+        new_node.next = start;
+        new_node.prev = last;
+  
+        // Update next and previous pointers of start
+        // and last.
+        last.next = (start).prev = new_node;
+  
+        // Update start pointer
+        start = new_node;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
