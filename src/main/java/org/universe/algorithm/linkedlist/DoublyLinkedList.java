9424ead9ada9361b022de5e2b64c0987db2a1779
Arrayq <arrayq@protonmail.com>
2022-11-14 09:16:24 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -958,6 +958,40 @@ public class DoublyLinkedList {
         next.prev = new_node;
     }
 
+    public void insertEnd(int value) {
+        // If the list is empty, create a single node
+        // circular and doubly list
+        if (start == null) {
+            Node new_node = new Node();
+            new_node.data = value;
+            new_node.next = new_node.prev = new_node;
+            start = new_node;
+            return;
+        }
+  
+        // If list is not empty
+  
+        /* Find last node */
+        Node last = (start).prev;
+  
+        // Create Node dynamically
+        Node new_node = new Node();
+        new_node.data = value;
+  
+        // Start is going to be next of new_node
+        new_node.next = start;
+  
+        // Make new node previous of start
+        (start).prev = new_node;
+  
+        // Make last previous of new node
+        new_node.prev = last;
+  
+        // Make new node next of old last
+        last.next = new_node;
+    }
+  
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
