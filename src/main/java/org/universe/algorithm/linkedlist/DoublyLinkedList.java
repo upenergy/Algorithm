459c1abf00060a6d1ad483c607e0c8c899fd0371
Arrayq <arrayq@protonmail.com>
2022-12-02 08:42:37 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -1197,6 +1197,30 @@ public class DoublyLinkedList {
         return last;
     }
 
+    public Node addAfter(Node last, int data, int item) {
+        if (last == null)
+            return null;
+    
+        Node temp, p;
+        p = last.next;
+        do {
+            if (p.data == item) {
+                temp = new Node();
+                temp.data = data;
+                temp.next = p.next;
+                p.next = temp;
+    
+                if (p == last)
+                    last = temp;
+                return last;
+            }
+            p = p.next;
+        } while (p != last.next);
+    
+        System.out.println(item + " not present in the list.");
+        return last;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
