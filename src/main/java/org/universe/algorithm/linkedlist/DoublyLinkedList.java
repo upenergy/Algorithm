b0383ad2654c634709d831a63aa8f766b625980a
Arrayq <arrayq@protonmail.com>
2022-12-04 11:02:09 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -1275,6 +1275,45 @@ public class DoublyLinkedList {
         }
     }
 
+    public void removeAllDuplicates() {
+     
+        // Create a dummy node that acts like a fake
+        // head of list pointing to the original head
+        Node dummy = new Node(0);
+    
+        // Dummy node points to the original head
+        dummy.next = head;
+        Node prev = dummy;
+        Node current = head;
+    
+        while (current != null)
+        {
+            // Until the current and previous values
+            // are same, keep updating current
+            while (current.next != null &&
+                prev.next.val == current.next.val)
+                current = current.next;
+    
+            // If current has unique value i.e current
+            // is not updated, Move the prev pointer
+            // to next node
+            if (prev.next == current)
+                prev = prev.next;
+    
+            // When current is updated to the last
+            // duplicate value of that segment, make
+            // prev the next of current*/
+            else
+                prev.next = current.next;
+    
+            current = current.next;
+        }
+    
+        // Update original head to the next of dummy
+        // node
+        head = dummy.next;
+    }
+    
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
