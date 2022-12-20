249e493af445758f6536f6536a5821f307a22b94
Arrayq <arrayq@protonmail.com>
2022-12-20 11:53:52 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -1559,6 +1559,47 @@ public class DoublyLinkedList {
         System.out.println();
     }
 
+    public void rotate(int k) {
+        // let us consider the example
+        // 10->20->30->40->50->60 - k=4
+        // initialising 2 nodes temp and last
+        Node last = head;
+        Node temp = head;
+         
+        // if head is null or k==0 no rotation is required
+        if (head == null || k == 0) {
+            return;
+        }
+ 
+        // Making last point to the last-node of the given
+        // linked list in this case 60
+        while (last.next != null) {
+            last = last.next;
+        }
+ 
+        // Rotating the linked list k times, one rotation at a
+        // time.
+        while (k != 0) {
+      
+            // Make head point to next of head
+            // so in the example given above head becomes 20
+            head = head.next;
+      
+            // Making next of temp as null
+            // In the above example :10->null
+            temp.next = null;
+      
+            // Making temp as last node
+            // (head)20->30->40->50->60->10(last)
+            last.next = temp;
+            last = temp;
+      
+            // Point temp to head again for next rotation
+            temp = head;
+            k--;
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
