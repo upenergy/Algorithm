d3cace9fd9a9be802536dcc45dbc0c29ac07c185
Arrayq <arrayq@protonmail.com>
2022-12-27 15:06:34 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -1724,6 +1724,28 @@ public class DoublyLinkedList {
         }
     }
 
+    public void removeDuplicate(node head) {
+        // Hash to store seen values
+        HashSet<Integer> hs = new HashSet<>();
+ 
+        /* Pick elements one by one */
+        node current = head;
+        node prev = null;
+        while (current != null) {
+            int curval = current.val;
+ 
+            // If current value is seen before
+            if (hs.contains(curval)) {
+                prev.next = current.next;
+            }
+            else {
+                hs.add(curval);
+                prev = current;
+            }
+            current = current.next;
+        }
+    }
+
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
