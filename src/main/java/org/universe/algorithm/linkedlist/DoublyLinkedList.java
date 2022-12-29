48ad5bac8c4e7e62b27ce645b37f9d9c65ee053f
Arrayq <arrayq@protonmail.com>
2022-12-29 11:15:13 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -1747,6 +1747,38 @@ public class DoublyLinkedList {
     }
 
 
+    public void skipMdeleteN( Node head, int M, int N) {
+        Node curr = head, t;
+        int count;
+    
+        // The main loop that traverses
+        // through the whole list
+        while (curr!=null)
+        {
+            // Skip M nodes
+            for (count = 1; count < M && curr != null; count++)
+                curr = curr.next;
+    
+            // If we reached end of list, then return
+            if (curr == null)
+                return;
+    
+            // Start from next node and delete N nodes
+            t = curr.next;
+            for (count = 1; count <= N && t != null; count++)
+            {
+                Node temp = t;
+                t = t.next;
+            }
+            
+            // Link the previous list with remaining nodes
+            curr.next = t;
+    
+            // Set current pointer for next iteration
+            curr = t;
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
