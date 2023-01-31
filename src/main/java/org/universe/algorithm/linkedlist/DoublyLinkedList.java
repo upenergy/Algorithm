8c6c0b7196dfdf33fac4871076fd7374c2c59278
Arrayq <arrayq@protonmail.com>
2023-01-31 13:16:37 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -2377,6 +2377,63 @@ public class DoublyLinkedList {
         }
     }
 
+
+    public void addToTheLast(Node node) {
+        if (head == null) {
+            head = node;
+        }
+        else {
+            Node temp = head;
+            while (temp.next != null)
+                temp = temp.next;
+            temp.next = node;
+        }
+    }
+
+
+    public Node sortedMerge(Node headA, Node headB)
+    {
+ 
+        /* a dummy first node to
+           hang the result on */
+        Node dummyNode = new Node(0);
+ 
+        /* tail points to the
+        last result node */
+        Node tail = dummyNode;
+        while (true) {
+ 
+            /* if either list runs out,
+            use the other list */
+            if (headA == null) {
+                tail.next = headB;
+                break;
+            }
+            if (headB == null) {
+                tail.next = headA;
+                break;
+            }
+ 
+            /* Compare the data of the two
+            lists whichever lists' data is
+            smaller, append it into tail and
+            advance the head to the next Node
+            */
+            if (headA.data <= headB.data) {
+                tail.next = headA;
+                headA = headA.next;
+            }
+            else {
+                tail.next = headB;
+                headB = headB.next;
+            }
+ 
+            /* Advance the tail */
+            tail = tail.next;
+        }
+        return dummyNode.next;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
