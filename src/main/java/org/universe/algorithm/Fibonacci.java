143ccd3f80cf288d4e3ba5ecf4da92e4647746b9
Arrayq <arrayq@protonmail.com>
2023-02-10 09:49:49 -0800

    Update Fibonacci.java

--- src/main/java/org/universe/algorithm/Fibonacci.java
+++ src/main/java/org/universe/algorithm/Fibonacci.java
@@ -18,14 +18,6 @@ public class Fibonacci {
         return f[n];
     }
 
-    public int fib(int n) {
-        int F[][] = new int[][]{{1,1},{1,0}};
-        if (n == 0)
-            return 0;
-        power(F, n-1);
-        
-        return F[0][0];
-    }
 
     public int fib(int n) {
         /* Declare an array to store Fibonacci numbers. */
@@ -45,6 +37,19 @@ public class Fibonacci {
         
         return f[n];
     }
+
+    public void multiply(int F[][], int M[][]) {
+        int x =  F[0][0]*M[0][0] + F[0][1]*M[1][0];
+        int y =  F[0][0]*M[0][1] + F[0][1]*M[1][1];
+        int z =  F[1][0]*M[0][0] + F[1][1]*M[1][0];
+        int w =  F[1][0]*M[0][1] + F[1][1]*M[1][1];
+        
+        F[0][0] = x;
+        F[0][1] = y;
+        F[1][0] = z;
+        F[1][1] = w;
+    }
+
       
     public static void main (String args[]) {
         int n = 9;
