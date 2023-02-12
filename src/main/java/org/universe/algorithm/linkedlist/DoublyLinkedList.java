9885ed0e7d85ba0e0143cdcbf5326b528bfa42ec
Arrayq <arrayq@protonmail.com>
2023-02-12 07:57:14 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -2545,6 +2545,57 @@ public class DoublyLinkedList {
         }
     }
 
+    public int maxNodeLevel(Node root) {
+        if (root == null)
+            return -1;
+    
+        Queue<Node> q = new LinkedList<Node> ();
+        q.add(root);
+    
+        // Current level
+        int level = 0;
+    
+        // Maximum Nodes at same level
+        int max = Integer.MIN_VALUE;
+    
+        // Level having maximum Nodes
+        int level_no = 0;
+    
+        while (true)
+        {
+            // Count Nodes in a level
+            int NodeCount = q.size();
+    
+            if (NodeCount == 0)
+                break;
+    
+            // If it is maximum till now
+            // Update level_no to current level
+            if (NodeCount > max)
+            {
+                max = NodeCount;
+                level_no = level;
+            }
+    
+            // Pop complete current level
+            while (NodeCount > 0)
+            {
+                Node Node = q.peek();
+                q.remove();
+                if (Node.left != null)
+                    q.add(Node.left);
+                if (Node.right != null)
+                    q.add(Node.right);
+                NodeCount--;
+            }
+    
+            // Increment for next level
+            level++;
+        }
+    
+        return levelNo;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
