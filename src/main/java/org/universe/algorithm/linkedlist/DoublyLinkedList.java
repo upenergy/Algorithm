89464c548a53fd535fbb2169b65f8df01cb5a5af
Arrayq <arrayq@protonmail.com>
2023-02-20 10:50:51 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -2764,6 +2764,51 @@ public class DoublyLinkedList {
         return false;
     }
 
+    public int minStepToReachTarget(int knightPos[],
+                                    int targetPos[], int N) {
+        // x and y direction, where a knight can move
+        int dx[] = { -2, -1, 1, 2, -2, -1, 1, 2 };
+        int dy[] = { -1, -2, -2, -1, 1, 2, 2, 1 };
+ 
+        // queue for storing states of knight in board
+        Queue<cell> q = new LinkedList<>();
+ 
+        // push starting position of knight with 0 distance
+        q.add(new cell(knightPos[0], knightPos[1], 0));
+ 
+        cell t;
+        int x, y;
+        boolean visit[][] = new boolean
+            [N + 1][N + 1]; // default initialized to false
+ 
+        // visit starting state
+        visit[knightPos[0]][knightPos[1]] = true;
+ 
+        // loop until we have one element in queue
+        while (!q.isEmpty()) {
+            t = q.poll();
+ 
+            // if current cell is equal to target cell,
+            // return its distance
+            if (t.x == targetPos[0] && t.y == targetPos[1])
+                return t.dis;
+ 
+            // loop for all reachable states
+            for (int i = 0; i < 8; i++) {
+                x = t.x + dx[i];
+                y = t.y + dy[i];
+ 
+                // If reachable state is not yet visited and
+                // inside board, push that state into queue
+                if (isInside(x, y, N) && !visit[x][y]) {
+                    visit[x][y] = true;
+                    q.add(new cell(x, y, t.dis + 1));
+                }
+            }
+        }
+        return Integer.MAX_VALUE;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
