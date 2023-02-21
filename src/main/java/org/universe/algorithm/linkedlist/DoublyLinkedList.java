8c7ef8a09218361d33eb893ff62ef12cb6381429
Arrayq <arrayq@protonmail.com>
2023-02-21 09:02:44 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -2808,6 +2808,55 @@ public class DoublyLinkedList {
         }
         return Integer.MAX_VALUE;
     }
+    
+    public Boolean isReachable(int s, int d){
+        LinkedList<Integer>temp;
+ 
+        // Mark all the vertices as not visited(By default set
+        // as false)
+        boolean visited[] = new boolean[V];
+ 
+        // Create a queue for BFS
+        LinkedList<Integer> queue = new LinkedList<Integer>();
+ 
+        // Mark the current node as visited and enqueue it
+        visited[s]=true;
+        queue.add(s);
+ 
+        // 'i' will be used to get all adjacent vertices of a vertex
+        Iterator<Integer> i;
+        while (queue.size()!=0)
+        {
+            // Dequeue a vertex from queue and print it
+            s = queue.poll();
+ 
+            int n;
+            i = adj[s].listIterator();
+ 
+            // Get all adjacent vertices of the dequeued vertex s
+            // If a adjacent has not been visited, then mark it
+            // visited and enqueue it
+            while (i.hasNext())
+            {
+                n = i.next();
+ 
+                // If this adjacent node is the destination node,
+                // then return true
+                if (n==d)
+                    return true;
+ 
+                // Else, continue to do BFS
+                if (!visited[n])
+                {
+                    visited[n] = true;
+                    queue.add(n);
+                }
+            }
+        }
+ 
+        // If BFS is complete without visited d
+        return false;
+    }
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
