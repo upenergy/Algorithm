76ff1ec1be75199c984a500c2fbbb3d2763811c5
Arrayq <arrayq@protonmail.com>
2023-03-03 10:09:23 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -3031,7 +3031,32 @@ public class DoublyLinkedList {
         }
         }
     }
-        
+    
+    public String FirstNonRepeating(String A) {
+          // Arraylist to keep track of current unique characters
+          // Hashmap to keep track of character encountered at least once
+          ArrayList<Character> list = new ArrayList<>();
+        HashMap<Character, Integer> map = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+ 
+        for (char ch : A.toCharArray()) {
+            if (!map.containsKey(ch)) { // any new character encountered first time
+                list.add(ch);
+                map.put(ch, 1);
+            }
+            else {
+                  //any repeated character encountered
+                int index = list.indexOf(ch);
+               
+                // for any repeated character encountered more than twice the
+                  // index will be -1
+                if (index != -1)
+                      list.remove(index);
+            }
+            sb.append(list.isEmpty() ? '#' : list.get(0));
+        }
+        return sb.toString();
+    }
 
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
