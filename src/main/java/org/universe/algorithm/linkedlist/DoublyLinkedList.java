ba2b075f762e3cdc5a1e951553b0f23692659dc0
Arrayq <arrayq@protonmail.com>
2023-03-08 12:51:04 -0800

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -3170,6 +3170,58 @@ public class DoublyLinkedList {
         return false;
     }
 
+    public void floodFill(int[][] screen, int m, int n, int x, int y, int prevC, int newC) {
+        Vector<Point> queue = new Vector<Point>();
+   
+        // Append the position of starting
+        // pixel of the component
+        queue.add(new Point(x, y));
+   
+        // Color the pixel with the new color
+        screen[x][y] = newC;
+   
+        // While the queue is not empty i.e. the
+        // whole component having prevC color
+        // is not colored with newC color
+        while(queue.size() > 0)
+        {
+            // Dequeue the front node
+            Point currPixel = queue.get(queue.size() - 1);
+            queue.remove(queue.size() - 1);
+   
+            int posX = currPixel.x;
+            int posY = currPixel.y;
+   
+            // Check if the adjacent
+            // pixels are valid
+            if(isValid(screen, m, n, posX + 1, posY, prevC, newC))
+            {
+                // Color with newC
+                // if valid and enqueue
+                screen[posX + 1][posY] = newC;
+                queue.add(new Point(posX + 1, posY));
+            }
+   
+            if(isValid(screen, m, n, posX-1, posY, prevC, newC))
+            {
+                screen[posX-1][posY]= newC;
+                queue.add(new Point(posX-1, posY));
+            }
+   
+            if(isValid(screen, m, n, posX, posY + 1, prevC, newC))
+            {
+                screen[posX][posY + 1]= newC;
+                queue.add(new Point(posX, posY + 1));
+            }
+   
+            if(isValid(screen, m, n, posX, posY-1, prevC, newC))
+            {
+                screen[posX][posY-1]= newC;
+                queue.add(new Point(posX, posY-1));
+            }
+        }
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
