7dab82c838c72dd5f5ecae06f81b55abb07ec8e2
Arrayq <arrayq@protonmail.com>
2023-03-21 03:23:11 -0700

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -3600,6 +3600,59 @@ public class DoublyLinkedList {
         }
     }
 
+    public int getMinDiceThrows(int move[], int n) {
+        int visited[] = new int[n];
+        Queue<qentry> q = new LinkedList<>();
+        qentry qe = new qentry();
+        qe.v = 0;
+        qe.dist = 0;
+ 
+        // Mark the node 0 as visited and enqueue it.
+        visited[0] = 1;
+        q.add(qe);
+ 
+        // Do a BFS starting from vertex at index 0
+        while (!q.isEmpty()) {
+            qe = q.remove();
+            int v = qe.v;
+ 
+            // If front vertex is the destination
+            // vertex, we are done
+            if (v == n - 1)
+                break;
+ 
+            // Otherwise dequeue the front vertex and
+            // enqueue its adjacent vertices (or cell
+            // numbers reachable through a dice throw)
+            for (int j = v + 1; j <= (v + 6) && j < n;
+                 ++j) {
+                // If this cell is already visited, then
+                // ignore
+                if (visited[j] == 0) {
+                    // Otherwise calculate its distance and
+                    // mark it as visited
+                    qentry a = new qentry();
+                    a.dist = (qe.dist + 1);
+                    visited[j] = 1;
+ 
+                    // Check if there a snake or ladder at
+                    // 'j' then tail of snake or top of
+                    // ladder become the adjacent of 'i'
+                    if (move[j] != -1)
+                        a.v = move[j];
+                    else
+                        a.v = j;
+                    q.add(a);
+                }
+            }
+        }
+ 
+        // We reach here when 'qe' has last vertex
+        // return the distance of vertex in 'qe'
+        return qe.dist;
+    }
+
+
     public class qentry {
         int v; // Vertex number
         int dist; // Distance of this vertex from source
