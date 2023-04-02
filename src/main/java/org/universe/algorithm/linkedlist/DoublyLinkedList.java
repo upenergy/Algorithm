80b23276651713da63dbefb721789d6a77fea02e
Arrayq <arrayq@protonmail.com>
2023-04-02 05:08:36 -0700

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -3848,6 +3848,46 @@ public class DoublyLinkedList {
         }
     }
 
+    public void increasingStack(int arr[], int N) {
+        // Initialise stack
+        stack<int> stk;
+    
+        for (int i = 0; i < N; i++) {
+    
+            // Either stack is empty or
+            // all bigger nums are popped off
+            while (stk.size() > 0 && stk.top() > arr[i]) {
+                stk.pop();
+            }
+            stk.push(arr[i]);
+        }
+    
+        int N2 = stk.size();
+        int ans[N2] = { 0 };
+        int j = N2 - 1;
+    
+        // Empty Stack
+        while (!stk.empty()) {
+            ans[j] = stk.top();
+            stk.pop();
+            j--;
+        }
+    
+        // Displaying the original array
+        cout << "The Array: ";
+        for (int i = 0; i < N; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    
+        // Displaying Monotonic increasing stack
+        cout << "The Stack: ";
+        for (int i = 0; i < N2; i++) {
+            cout << ans[i] << " ";
+        }
+        cout << endl;
+    }
+
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
