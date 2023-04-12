b53de56ea1398535c6f4a2f087e457ebffc2ea95
Arrayq <arrayq@protonmail.com>
2023-04-12 09:52:45 -0700

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -3996,6 +3996,24 @@ public class DoublyLinkedList {
         return this.array[this.rear];
     }
 
+    public void enqueue(int key) {
+ 
+        // Create a new LL node
+        QNode temp = new QNode(key);
+ 
+        // If queue is empty, then new node is front and
+        // rear both
+        if (this.rear == null) {
+            this.front = this.rear = temp;
+            return;
+        }
+ 
+        // Add the new node at the end of queue and change
+        // rear
+        this.rear.next = temp;
+        this.rear = temp;
+    }
+    
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
  
