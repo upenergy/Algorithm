ca8d4c5f993f222b4fd94c8d99c57a2f140720cb
Arrayq <arrayq@protonmail.com>
2023-04-14 10:18:39 -0700

    Update DoublyLinkedList.java

--- src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
+++ src/main/java/org/universe/algorithm/linkedlist/DoublyLinkedList.java
@@ -4029,6 +4029,44 @@ public class DoublyLinkedList {
         if (this.front == null)
             this.rear = null;
     }
+
+    public void KMPSearch(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+ 
+        // create lps[] that will hold the longest
+        // prefix suffix values for pattern
+        int lps[] = new int[M];
+        int j = 0; // index for pat[]
+ 
+        // Preprocess the pattern (calculate lps[]
+        // array)
+        computeLPSArray(pat, M, lps);
+ 
+        int i = 0; // index for txt[]
+        while ((N - i) >= (M - j)) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                System.out.println("Found pattern "
+                                   + "at index " + (i - j));
+                j = lps[j - 1];
+            }
+ 
+            // mismatch after j matches
+            else if (i < N
+                     && pat.charAt(j) != txt.charAt(i)) {
+                // Do not match lps[0..lps[j-1]] characters,
+                // they will match anyway
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i = i + 1;
+            }
+        }
+    }
     
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
